08fa39feb1d90aa29ba42a3caf29f30436e105a5
Louis-Olivier Major <louis.olivier.major@gmail.com>
2026-01-02 10:41:26 -0500

    Angular forms done

--- src/app/housing.service.ts
+++ src/app/housing.service.ts
@@ -116,4 +116,9 @@ export class HousingService {
   getHousingLocationById(id: number): HousingLocationInfo | undefined {
     return this.housingLocationList.find((housingLocation) => housingLocation.id === id);
   }
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(
+      `Home application received: firstName: ${firstName}, lastName: ${lastName}, email: ${email}`
+    );
+  }
 }
